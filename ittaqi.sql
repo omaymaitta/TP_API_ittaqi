e509e6bd49509c31459d9c92a96e98a960553332
omaymaitta <62363101+omaymaitta@users.noreply.github.com>
2021-06-20 12:10:34 +0100

    Add files via upload

---
+++ ittaqi.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : Dim 20 juin 2021 à 12:57
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `ittaqi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+-- Erreur de lecture de structure pour la table ittaqi.articles : #1932 - Table 'ittaqi.articles' doesn't exist in engine
+-- Erreur de lecture des données pour la table ittaqi.articles : #1064 - Erreur de syntaxe près de 'FROM `ittaqi`.`articles`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articletags`
+--
+-- Erreur de lecture de structure pour la table ittaqi.articletags : #1932 - Table 'ittaqi.articletags' doesn't exist in engine
+-- Erreur de lecture des données pour la table ittaqi.articletags : #1064 - Erreur de syntaxe près de 'FROM `ittaqi`.`articletags`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articletags2`
+--
+-- Erreur de lecture de structure pour la table ittaqi.articletags2 : #1932 - Table 'ittaqi.articletags2' doesn't exist in engine
+-- Erreur de lecture des données pour la table ittaqi.articletags2 : #1064 - Erreur de syntaxe près de 'FROM `ittaqi`.`articletags2`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comments`
+--
+-- Erreur de lecture de structure pour la table ittaqi.comments : #1932 - Table 'ittaqi.comments' doesn't exist in engine
+-- Erreur de lecture des données pour la table ittaqi.comments : #1064 - Erreur de syntaxe près de 'FROM `ittaqi`.`comments`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tags`
+--
+-- Erreur de lecture de structure pour la table ittaqi.tags : #1932 - Table 'ittaqi.tags' doesn't exist in engine
+-- Erreur de lecture des données pour la table ittaqi.tags : #1064 - Erreur de syntaxe près de 'FROM `ittaqi`.`tags`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+-- Erreur de lecture de structure pour la table ittaqi.users : #1932 - Table 'ittaqi.users' doesn't exist in engine
+-- Erreur de lecture des données pour la table ittaqi.users : #1064 - Erreur de syntaxe près de 'FROM `ittaqi`.`users`' à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
